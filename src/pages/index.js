1a7a87fe93d0801939f42c0f33b042d5f7a42a24
Jip Vermeer <Jip.Vermeer@hotmail.com>
2021-06-17 16:38:16 +0200

    Finished Gatsby Tutorial Part 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,15 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage 
+      alt="Me sitting on a bench with a friend, throwing a peace sign at a sheep"
+      src="../images/1623940352219.jpg"
+      />
     </Layout>
   )
 }
